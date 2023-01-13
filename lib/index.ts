ce125944ea9189d4a83ce354be9bdba40ed14533
Sachin Shekhar <noreply.git@sachinshekhar.com>
2023-01-14 01:27:20 +0530

    Added region list helper

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export { StackSet, StackSetProps } from './src/stack-set';
 export { Stage, StageProps } from './src/stage';
 export { Region } from './src/region';
+export { Regions } from './src/regions';
