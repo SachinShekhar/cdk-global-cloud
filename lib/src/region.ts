19d0f24174226fa21a8cb48be9006385e3cdc4b0
Sachin Shekhar <noreply.git@sachinshekhar.com>
2023-01-21 19:47:15 +0530

    Beautified generated regional stack name

--- lib/src/region.ts
+++ lib/src/region.ts
@@ -96,3 +96,36 @@ export class Regions {
 
   static readonly GOVERNMENT: Region[] = ['us-gov-east-1', 'us-gov-west-1'];
 }
+
+export function getRegionName(region: Region): string {
+  return (<{ [K in Region]: string }>{
+    'us-east-1': 'NorthVirginia',
+    'us-east-2': 'Ohio',
+    'us-west-1': 'NorthCalifornia',
+    'us-west-2': 'Oregon',
+    'ca-central-1': 'Canada',
+    'eu-central-1': 'Frankfurt',
+    'eu-central-2': 'Zurich',
+    'eu-north-1': 'Stockholm',
+    'eu-west-1': 'Ireland',
+    'eu-west-2': 'London',
+    'eu-west-3': 'Paris',
+    'eu-south-1': 'Milan',
+    'eu-south-2': 'Spain',
+    'ap-east-1': 'HongKong',
+    'ap-south-1': 'Mumbai',
+    'ap-south-2': 'Hyderabad',
+    'ap-southeast-1': 'Singapore',
+    'ap-southeast-2': 'Sydney',
+    'ap-southeast-3': 'Jakarta',
+    'ap-northeast-1': 'Tokyo',
+    'ap-northeast-2': 'Seoul',
+    'ap-northeast-3': 'Osaka',
+    'sa-east-1': 'SaoPaulo',
+    'me-central-1': 'UAE',
+    'me-south-1': 'Bahrain',
+    'af-south-1': 'CapeTown',
+    'us-gov-east-1': 'USGovCloudEast',
+    'us-gov-west-1': 'USGovCloudWest',
+  })[region];
+}
