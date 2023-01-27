21bb4811cb6757da8a0f44c6c69aa38f4845f238
Sachin Shekhar <noreply.git@sachinshekhar.com>
2023-01-28 04:16:51 +0530

    Added tests

--- tests/api-global-cloud.test.ts
+++ tests/api-global-cloud.test.ts
@@ -1,22 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-
-import { ApiGlobalCloud } from '../examples/multi-region-serverless/api-global-cloud';
-import { Stage } from '../lib';
-
-const app = new cdk.App();
+import { prodAPICloud } from '../examples/multi-region-serverless';
 
 describe('API Global Cloud', () => {
   test('synthesizes correctly', () => {
-    const apiGlobalCloud = new ApiGlobalCloud(
-      app,
-      new Stage('Dev', { account: '0123456789' })
-    );
-
-    const globalTemplate = Template.fromStack(apiGlobalCloud.stacks[0]);
-    const regionalTemplate = Template.fromStack(apiGlobalCloud.stacks[1]);
-
-    expect(globalTemplate.toJSON()).toMatchSnapshot();
-    expect(regionalTemplate.toJSON()).toMatchSnapshot();
+    prodAPICloud.stacks.forEach((stack) => {
+      const template = Template.fromStack(stack);
+      expect(template.toJSON()).toMatchSnapshot();
+    });
   });
 });
